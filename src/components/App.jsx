551a52407b0b2362960e3bebd89ea148cfdd006e
dmytro-marchuk <darthwudoodoodoo@gmail.com>
2023-05-04 22:41:16 +0300

    routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,30 @@
 import { NavLink, Route, Routes } from 'react-router-dom';
-import Home from './Home/Home';
+import Home from '../pages/Home/Home';
+import Movies from 'pages/Movies/Movies';
+import styled from 'styled-components';
+
+const StyledLink = styled(NavLink)`
+  color: black;
+
+  &.active {
+    color: orange;
+  }
+`;
 
 export const App = () => {
   return (
     <>
-      <nav>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/movies">Movies</NavLink>
-      </nav>
+      <header>
+        <nav>
+          <StyledLink to="/">Home</StyledLink>
+          <StyledLink to="/movies">Movies</StyledLink>
+        </nav>
+      </header>
       <Routes>
-        <Route path="/" element={<div>HomePage</div>} />
-        <Route path="movies" element={<div>Movies</div>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:movieId" element={<div>Movie Details</div>} />
+        <Route path="*" element={<div>Page not found!</div>} />
       </Routes>
     </>
   );
